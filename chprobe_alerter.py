8abca83d5eb9aff553dfbfecde35eb2a25ccaf0a
lordberre <jakerlock@gmail.com>
2018-09-02 21:05:06 +0200

    code cleanup + more ood, fix for alerts with multiple probes, temp removed email notifications

--- chprobe_alerter.py
+++ chprobe_alerter.py
@@ -4,11 +4,12 @@ from flask import request, session
 import urllib.request
 import time 
 import yaml
+from uuid import uuid4
 
-# TODO
-# Make class for alert_ vars instead
-# Issues: Email notify sends multiple attachments
-# Handling of multiple probes in evalMatches list.. Currently only takes index 0
+# Changelog since commit #6c4963d:
+# 1. Email notifcations temporarily removed.
+# 2. Fixed handling of multiple alerting probes
+# 3. Code cleanup + more OOD
 
 # Load config file
 with open('alerter_settings.yaml') as f:
@@ -37,32 +38,45 @@ except:
 # icmp deadline
 icmp_deadline = 100
 
+# Class used to create object for incoming alerts 
+class Alert:
+    def __init__(self, data, probes='not-available'):
+        self.created = time.strftime("%Y-%m-%d %H:%M")
+        self.id = uuid4()
+        self.data = data
+        self.value = None
+        for key, value in data.items():
+            setattr(self, key, value)
+        if self.evalMatches:
+            self.alertdetails = self.evalMatches
+            self.probes = {}
+            for index in self.alertdetails:
+                self.probes[index['metric']] = index['value']
+        else:
+            self.probes = probes
+
+    def prettyvalue(self,extra_text=""):
+        returned_values = []
+        for index in self.alertdetails:
+            prettyvalue = float(format(index['value'], '.2f'))
+            returned_values.append(str(prettyvalue) + extra_text)
+        return(', '.join(returned_values))
+
+    def listprobes(self,o_format='list'):
+        if 'pretty' in o_format:
+            return((', '.join(self.probes.keys())))
+        elif 'list' in o_format:
+            return(list(self.probes.keys()))
+
 def load_graph(url):
     urllib.request.urlretrieve(url, 'test.png')
-#   r = urllib.request.urlopen(url)
-#   img = r.read()
-#   with open('img.png', 'wb') as ofile:
-#       ofile.write(img)
-#   return img  # img never gets defined when function is called in the decorator...
     return
 
 def fetch_image(url, urldir):
     """ Fetch string after delimiter (webdir/) """
     return url.partition(urldir)[2]
 
-if email_notify:
-    import smtplib
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-    from email.mime.image import MIMEImage
-    strFrom = sender_address
-    strTo = receiver_address
-    msgRoot = MIMEMultipart('related')
-    msgRoot['Subject'] = subject
-    msgRoot['From'] = strFrom
-    msgRoot['To'] = strTo
-
-elif webhook_notify:
+if webhook_notify:
     import requests
 
 app = Flask(__name__)
@@ -71,34 +85,35 @@ app.secret_key = "something-from-os.urandom(24)"
 @app.route('/alert', methods = ['POST'])
 def postJsonHandler():
     print ('Is json? ',request.is_json) if verbose else None
-    alert_localtime = time.strftime("%Y-%m-%d %H:%M")
-    d = request.get_json()
-    rule_url = d['ruleUrl']
+    try:
+        d = request.get_json()
+    except:
+        raise('Incorrect JSON provided')
+
+#   Create object from json data
+    alert = Alert(d)
 #    alerting_probe = d['evalMatches'][0]['metric']
-    alert_state = d['state']
-    alert_title = d['title']
-    alert_rulename = d['ruleName']
-    chart_url = d['imageUrl']
+    chart_url = alert.imageUrl
     print(chart_url) if verbose else None
     print (d) if verbose else None
-    print('Recieved alert state:',alert_state) if verbose else None
+    print('Recieved alert state:',alert.state) if verbose else None
 
 # Parse settings from message
 # Script should still function without message settings.
     try:
-        alert_settings = d['message']
+        alert_settings = alert.message
         message = True
    
-    except KeyError: 
+    except (AttributeError, KeyError) as e: 
         message = False
-        print('Warning: No message key was detected in the alert')
+        print('Warning: No message key was detected in the alert:', e)
     
     if message:
         try:
             if isinstance(alert_settings,str):
-                alert_settings = eval(d['message'])
+                alert_settings = eval(alert.message)
             else:
-                alert_settings = d['message']
+                alert_settings = alert.message
             icmp_size = alert_settings['icmp_size'] if 'icmp_size' in alert_settings else None
             icmp_interval = alert_settings['icmp_interval'] if 'icmp_interval' in alert_settings else None
         except NameError:
@@ -107,118 +122,49 @@ def postJsonHandler():
 
     print(chart_url) if verbose else None
     print (d) if verbose else None
-    print('Recieved alert state:',alert_state) if verbose else None
+    print('Recieved alert state:',alert.state) if verbose else None
 
 # Check if alert is a grafana test
-    if 'Test notification' in alert_rulename:
+    if 'Test notification' in alert.ruleName:
         test_alert = True
     else:
         test_alert = False
 
 # If alert state is 'No data' or 'OK' just drop them unless we're just testing
-    if alert_state in ('ok', 'no_data'):
-        print('Dropped request with state:', alert_state)
+    if alert.state in ('ok', 'no_data'):
+        print('Dropped request with state:', alert.state)
         return 'Dropped request due to state'
 
-    elif 'alerting' in alert_state:
+    elif 'alerting' in alert.state:
         if test_alert: # Use dummy data if testing
-            alert_probe = 'test_notification'
-            alert_value = 'test_value'
+            alert.probes = 'test_notification'
+            alert.value = 'test_value'
             alert_hvalue = 'ms'
-            print(alert_probe, alert_value) if verbose else None
+            print(alert.probes, alert.prettyvalue()) if verbose else None
 
         else: # Parse probe name and value
-            alert_probe = d['evalMatches'][0]['metric']
-            alert_value = d['evalMatches'][0]['value']
-
-            # Round to two deicmals max
-            alert_value = float(format(alert_value, '.2f'))
-            print(alert_probe, alert_value) if verbose else None
 
             # Add some claryfing text to the values
-            if 'Max RTT' in alert_title:
-                alert_hvalue = str(alert_value) + 'ms RTT (spike)'
+            if 'Max RTT' in alert.title:
+                alert_hvalue = alert.prettyvalue('ms RTT (spike)')
 
-            elif 'Average RTT' in alert_title:
-                alert_hvalue = str(alert_value) + 'ms RTT (avg)'
-                  
-            elif 'Loss' in alert_title:
+            elif 'Average RTT' in alert.title:
+                alert_hvalue = alert.prettyvalue('ms RTT (avg)')
+                        
+            elif 'Loss' in alert.title:
             # Check interval time from message field and use it to measure outage time
+            # Fetches the highest value if multiple
                 if icmp_deadline == 100 and message:
-                    outage_s = icmp_interval * alert_value
-                    alert_hvalue = str(alert_value) + '% packetloss (~{} seconds)'.format(int(outage_s))
+                    outage_s = icmp_interval * max(alert.prettyvalue())
+                    alert_hvalue = alert.prettyvalue('% packetloss (~{} seconds)').format(int(outage_s))
                 else:
-                    alert_hvalue = str(alert_value) + '% packetloss'
+                    alert_hvalue = alert.prettyvalue('% packetloss')
 
     else:
-        print('Dropped request with state:', alert_state)
+        print('Dropped request with state:', alert.state)
         return 'Dropped request due to state'
 
-    if email_notify:
-# If image url is embedded in alert, parse the filename and open the file directly since it's on this local machine
-        if chart_url:
-
-   # For debugging/testing, accept the testalerter img in the same way
-            if grafana_testurl in chart_url:
-
-    # ... And DL the image instead of webdav
-                urllib.request.urlretrieve(chart_url, 'mixed_styles.png')
-                final_image = fetch_image(chart_url,'/blog/')
-            
-            else:
-                chart_png = fetch_image(chart_url,'/webdav/')
-                final_image = webdav_path + chart_png
-                print(final_image)
-
-# Otherwise skip attaching image in the notifier
-# ....
-        else:
-           print('No image found in POST, so nothing to attach')  
-
-# Create and send the email
-        try:
-            msgRoot.preamble = 'This is a multi-part message in MIME format.'
-            msgAlternative = MIMEMultipart('alternative')
-            msgRoot.attach(msgAlternative)
-            msgText = MIMEText('This is the alternative plain text message.')
-            msgAlternative.attach(msgText)
-#            msgText = MIMEText('<b>Alert for a probe <i>HTML</i> text</b> and an image.<br><img src="cid:image1"><br>Nifty!', 'html')
-            mail_body = """\
-<p>Alert for: """ + alert_title + """<br>
-<br>
-Alert trigged at: """ + alert_localtime + """.<br>
-Alert reason: """ + 'somereason' + """.<br>
-<br>
-Graph:<br>
-<br>
-<img src="cid:image1">
-</p>
-"""
-            msgText = MIMEText(mail_body, 'html')
-            msgAlternative.attach(msgText)
-
-            fp = open(final_image, 'rb')
-            msgImage = MIMEImage(fp.read())
-            fp.close()
-            msgImage.add_header('Content-ID', '<image1>')
-            msgRoot.attach(msgImage)
-
-# Send the email (this example assumes SMTP authentication is required)
-            smtp = smtplib.SMTP()
-            smtp.connect(smtp_server)
-            smtp.sendmail(strFrom, strTo, msgRoot.as_string())
-            smtp.quit()
-            print ("Successfully sent email")
-#        except SMTPException:
-        except:
-            pass
-#            print ("Error: unable to send email")
-        finally:
-# Empty the body for the next alert
-            session.clear()
-            print('.')
-
-    elif webhook_notify:
+    if webhook_notify:
 
         if grafana_testurl in chart_url:
             final_image = chart_url
@@ -230,19 +176,19 @@ Graph:<br>
             chart_url = '<no image available>'
         
 # Create the POST
-        if 'alerting' in alert_state:
+        if 'alerting' in alert.state:
             alert_text = \
 'Alert from chprobe at {}. Title: {} \n\
 Alert state: {} \n\
-Probe: {} \n\
+Probe(s): {} \n\
 Alert value: {} \n\
-Check Grafana for further info: {} \n'.format(alert_localtime, alert_title, alert_state, alert_probe, alert_hvalue, rule_url)
+Check Grafana for further info: {} \n'.format(alert.created, alert.title, alert.state, alert.listprobes('pretty'), alert_hvalue, alert.ruleUrl)
 
         else:
             alert_text = \
 'Alert from chprobe at {}. Title: {} \n\
 Alert state: {} \n\
-Check Grafana for further info: {} \n'.format(alert_localtime, alert_title, alert_state, rule_url)
+Check Grafana for further info: {} \n'.format(alert.created, alert.title, alert.state, alert.ruleUrl)
         markdown_newline = '  '
         dict_in = dict()
         dict_in['text'] = alert_text + '![alertgraph]' + '(' + final_image + ')'
@@ -256,4 +202,4 @@ Check Grafana for further info: {} \n'.format(alert_localtime, alert_title, aler
             print('What we sent: ', json_input)
     return 'JSON posted'
  
-app.run(host='127.0.0.1', port= 8091, debug=False)
+app.run(host='127.0.0.1', port= 8092, debug=False)
